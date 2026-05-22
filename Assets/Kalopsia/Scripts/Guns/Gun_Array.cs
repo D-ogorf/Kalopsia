62ca3487c32fe51cde88611b2ab667699abe8314
D-ogorf <ferreira.r.diogo2009@gmail.com>
2026-05-22 18:31:45 +0100

    Update Gun_Array.cs

--- Assets/Kalopsia/Scripts/Guns/Gun_Array.cs
+++ Assets/Kalopsia/Scripts/Guns/Gun_Array.cs
@@ -5,36 +5,36 @@ using Unity.VisualScripting;
 using UnityEngine;
 
 [System.Serializable]
-    public class Weapon
-    {
-        public string weaponName;
-        public float minimumFireRate;
-        public float maximumFireRate;
-        public float shotsToBuildUp;
-        public bool _isAuto;
+public class Weapon
+{
+    public string weaponName;
+    public float minimumFireRate;
+    public float maximumFireRate;
+    public float shotsToBuildUp;
+    public bool _isAuto;
 
-        public bool _hasWeapon;
-        public float timeSinceLastAtack;
-        public float curFireRate;
+    public bool _hasWeapon;
+    public float timeSinceLastAtack;
+    public float curFireRate;
 
-        public Bullets[] bullets;
-    }
-    [System.Serializable]
-    public class Bullets
-    {
-        public float dmg;
-        public int dmgType;
-        public int ammoType;
-        public int ammoCost;
-        public float speed;
-        public float delay;
-        public float rotation;
-        public float innacuracy;
+    public Bullets[] bullets;
+}
+[System.Serializable]
+public class Bullets
+{
+    public float dmg;
+    public int dmgType;
+    public int ammoType;
+    public int ammoCost;
+    public float speed;
+    public float delay;
+    public float rotation;
+    public float innacuracy;
 
-        public GameObject bulletFX;
-        public ParticleSystem blastFX;
-        public GameObject spawnPoint;
-    }
+    public GameObject bulletFX;
+    public ParticleSystem blastFX;
+    public GameObject spawnPoint;
+}
 public class Gun_Array : MonoBehaviour
 {
     public Weapon[] weapons;
