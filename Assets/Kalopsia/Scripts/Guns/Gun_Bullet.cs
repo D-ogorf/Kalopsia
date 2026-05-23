434e6045d07f48f14896c0e15bb2911d4df4e397
D-ogorf <ferreira.r.diogo2009@gmail.com>
2026-05-23 11:57:00 +0100

    more gun

--- Assets/Kalopsia/Scripts/Guns/Gun_Bullet.cs
+++ Assets/Kalopsia/Scripts/Guns/Gun_Bullet.cs
@@ -1,21 +1,29 @@
+using Mono.Cecil.Cil;
 using UnityEngine;
 
 public class Gun_Bullet : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float dmg;
+    public float dmgType;
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Enemy") ApplyOnHitFX();
+        DestroySelf();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Enemy") ApplyOnHitFX();
+        DestroySelf();
+    }
+
+    private void ApplyOnHitFX()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void DestroySelf()
     {
-        
-    }
-
-    void OnTriggerStay2D(Collider2D collision)
-    {
-        if(collision.gameObject.tag == "KillBox") Destroy(gameObject);
+        Destroy(gameObject);
     }
 }
