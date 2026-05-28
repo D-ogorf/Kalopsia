a2481dca461b423eecdae5389ce1c73aca6d993b
D-ogorf <ferreira.r.diogo2009@gmail.com>
2026-05-28 19:49:41 +0100

    gun

--- Assets/Kalopsia/Scripts/Guns/Gun_Pos.cs
+++ Assets/Kalopsia/Scripts/Guns/Gun_Pos.cs
@@ -4,24 +4,44 @@ public class Gun_Pos : MonoBehaviour
 {
     public sbyte leftRightInt;
     public sbyte upDownInt;
+    public Vector2 distFromCenter;
     [HideInInspector] public Set_mCh settings;
+    [HideInInspector] public mCh_Mov player;
 
     void Start()
     {
-        
+        this.player = GetComponentInParent<mCh_Mov>();
     }
 
     void Update()
     {
         StateChecker();
+        PosChange();
+        RotChange();
     }
 
     private void StateChecker()
     {
-        if(Input.GetKey(this.settings.left)) this.leftRightInt = -1;
+        if(!this.player._isDashing)
+        {
+            ChangeUpDown();
+            ChangeLeftRight();
+        }
+    }
 
-        if(Input.GetKey(this.settings.right)) this.leftRightInt = 1;
+    private void ChangeLeftRight()
+    {
+        if(this.upDownInt != 0 && this.player.leftRightInt == 0)
+        {
+            this.leftRightInt = 0;
+            return;
+        }
 
+        this.leftRightInt = this.player.lastLeftRight;
+    }
+
+    private void ChangeUpDown()
+    {
         if(!Input.GetKey(this.settings.up) && !Input.GetKey(this.settings.down))
         {
             this.upDownInt = 0;
@@ -31,5 +51,18 @@ public class Gun_Pos : MonoBehaviour
         if(Input.GetKey(this.settings.up)) this.upDownInt = 1;
 
         if(Input.GetKey(this.settings.down)) this.upDownInt = -1;
+    }
+
+    private void PosChange()
+    {
+        this.transform.localPosition = new Vector2(this.distFromCenter.x * this.leftRightInt, this.distFromCenter.y * this.upDownInt).normalized;
+    }
+
+    private void RotChange()
+    {
+        Vector2 Posdiff = this.player.transform.position - this.transform.position;
+        Posdiff.Normalize();  
+        float rotZ = Mathf.Atan2(Posdiff.y, Posdiff.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotZ);
     }
 }
